032b4707e67a44c0149120b7a6d3861ca60fafa5
Kapelianovych Yevhen <kapelianovych.y.v@gmail.com>
2021-05-05 09:57:00 +0300

    Separate content of .env file into variable and add DEBUG environment variable.

--- src/env_file.ts
+++ src/env_file.ts
@@ -1,7 +1,14 @@
 import { join } from 'path';
 import { promises } from 'fs';
 
+const envContent = `
+HOST=http://localhost:3000
+# Uncomment line below to get debug messages from Eleventy
+# and plugins.
+# DEBUG=*
+`;
+
 export const generateEnvFile = (cwd: string) =>
-  promises.writeFile(join(cwd, '.env'), 'HOST=http://localhost:3000', {
+  promises.writeFile(join(cwd, '.env'), envContent, {
     encoding: 'utf-8',
   });
